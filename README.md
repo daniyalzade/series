a99e7e72f38736d8f8c67df67467b40296b1b72f
eytan <eytan@chartbeat.com>
2015-05-06 11:39:56 -0700

    initial commit, start stroring weather data

---
+++ README.md
@@ -0,0 +1,4 @@
+## Next Steps
+* Add pranker
+* Look into AWS Lambda
+* Look into bitbucket
---
+++ weather.py
@@ -0,0 +1,29 @@
+import calendar
+from influxdb import InfluxDBClient
+import requests
+import time
+import ujson
+from utensils import dictutils
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from env import CREDS
+
+ts = calendar.timegm(time.gmtime())
+response = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%2012589342&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
+obj = ujson.loads(response.content)
+temp = dictutils.get_dotted(obj, 'query.results.channel.item.condition.temp')
+
+client = InfluxDBClient(**CREDS)
+
+json_body = [{
+        "points": [
+            ["new york", ts, temp],
+            ],
+        "name": "weather",
+        "columns": ["location", "timestamp", "temperature"]
+        }]
+#print client.write_points(json_body)
+result = client.query('select * from weather;')
+#print("Result: {0}".format(result))
