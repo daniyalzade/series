0d0536e9833b9a1e28e7f4ec0b1e543a11bca17b
eytan <eytan@chartbeat.com>
2015-05-12 21:26:34 -0700

    start using time instead of timestamp

--- weather.py
+++ weather.py
@@ -1,27 +1,22 @@
 import calendar
-from influxdb import InfluxDBClient
 import requests
 import time
 import ujson
 from utensils import dictutils
 
-import warnings
-warnings.filterwarnings("ignore")
-
-from env import CREDS
+from db import get_client
 
 ts = calendar.timegm(time.gmtime())
 response = requests.get('https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%2012589342&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys')
 obj = ujson.loads(response.content)
-temp = dictutils.get_dotted(obj, 'query.results.channel.item.condition.temp')
+temp = int(dictutils.get_dotted(obj, 'query.results.channel.item.condition.temp'))
 
-client = InfluxDBClient(**CREDS)
+client = get_client()
 
 json_body = [{
         "points": [
             ["new york", ts, temp],
             ],
         "name": "weather",
-        "columns": ["location", "timestamp", "temperature"]
+        "columns": ["location", "time", "temperature"]
         }]
-client.write_points(json_body)
